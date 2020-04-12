2d27a9a545042ab145a1cfcbeda1eac6ae14d06a
z-carlson <zcarlson87@gmail.com>
2020-04-12 12:20:18 -0500

    Updates

---
+++ precache-manifest.c5012d127c612a407f837f9a48009d66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14712e20cc163fb4b6ae68cf2fa8775",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3a89509558486d66239d",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "6754a96f777ba89af41c",
+    "url": "/robofriends/static/css/main.c3e4b138.chunk.css"
+  },
+  {
+    "revision": "3a89509558486d66239d",
+    "url": "/robofriends/static/js/2.2f7a48c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.2f7a48c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6754a96f777ba89af41c",
+    "url": "/robofriends/static/js/main.03c5727a.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
